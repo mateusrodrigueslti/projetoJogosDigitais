1abcd8ce7b810966000b758904f864f71035e2ab
Mateus Rodrigues <omateus2004@gmail.com>
2017-04-09 17:00:06 -0300

    Primeiro versionamento

---
+++ Assets/Movimento.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour {
+
+    public float velocidade = 1;
+    public float forcaDoPulo = 1;
+    public Vector3 axis;
+    Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(axis.x * horizontal , axis.y * vertical, velocidade));
+
+        if (Input.GetButtonDown("Jump")) {
+            rigid.AddForce(new Vector3(0,forcaDoPulo,0), ForceMode.Impulse);
+        }
+	}
+}
